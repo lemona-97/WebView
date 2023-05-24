9e6246ac06aa012cff254cc93cb7c86b5997fea3
lemona-97 <wcbe9745@naver.com>
2023-05-24 15:56:28 +0900

    WebView callback method test1

--- WebView/ViewController.swift
+++ WebView/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import WebKit
 
-final class ViewController: UIViewController {
+final class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
 
     
     @IBOutlet weak var webVIew: WKWebView!
@@ -18,15 +18,46 @@ final class ViewController: UIViewController {
         setUpWebView()
     }
     private func setUpWebView() {
-        self.webVIew.load(URLRequest(url: URL(string: "https://www.naver.com")!))
-        
+        self.webVIew.load(URLRequest(url: URL(string: "https://www.op.gg/")!))
+        self.webVIew.uiDelegate = self
+        self.webVIew.navigationDelegate = self
+        self.webVIew.allowsBackForwardNavigationGestures = true
+
     }
 
 }
 
-extension ViewController: WKUIDelegate {
-  
-}
-extension ViewController: WKNavigationDelegate {
+extension ViewController {
+    //웹뷰 생성 +(코드 베이스로 만들때 쓸듯)
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+        
+        WKWebView()
+    }
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo) async {
+        let ac = UIAlertController(title: "Henry", message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Good", style: .default, handler: nil))
+        present(ac, animated: true)
+    }
+    //    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo) async -> Bool {
+//        <#code#>
+//    }
+//    func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (String?) -> Void) {
+//        <#code#>
+//    }
+    
     
 }
+extension ViewController {
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        print("뷰 불러오는중임 ")
+    }
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        print("콘텐츠 받아오는중 ")
+    }
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("\(String(describing: navigation)) 끗")
+    }
+    
+}
+
